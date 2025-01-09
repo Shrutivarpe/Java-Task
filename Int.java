7704456200f669c1ce2169d464c1dfc76089f3f1
Shruti Varpe <shrutivarpe193@gmail.com>
2025-01-09 12:06:20 +0530

    add some changes

--- Int.java
+++ Int.java
@@ -3,21 +3,17 @@ public class Int {
     public static void main(String[] args) {
         int x = 10;
         int y = 30;
-        if (x>y){
-        System.out.println(y);
+        if (x > y) {
+            System.out.println(y);
 
-        System.out.println("x is greater than y");}
-        
-        else if(x<y)
-            
-        {System.out.println("x is less than y");
-        
-        
-        
-        }else{
-            System.out.println("Both are same");
+            System.out.println("x is greater than y");
         }
-        
+
+        else if (x < y){
+            System.out.println("x is less than y");
+
+        }
+
     }
-    
+
 }
